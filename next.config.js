190d61c367f816823261076fd51126fbcc0027b2
Lnx <46533182+lnxAv@users.noreply.github.com>
2022-08-05 14:55:57 -0400

    Webpack audio & shader support

--- next.config.js
+++ next.config.js
@@ -14,6 +14,34 @@ const { i18n } = require('./next-i18next.config');
  * Analzer: https://www.npmjs.com/package/@next/bundle-analyzer
  */
 const nextConfig = {
+  webpack(config, { isServer }) {
+    // audio support
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    })
+    // shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
+
+    return config
+  },
   reactStrictMode: true,
   swcMinify: true,
   i18n,
