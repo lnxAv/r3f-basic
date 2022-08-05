d05f457b8aab925d10409a97d0b5d4d06ad100ed
Lnx <46533182+lnxAv@users.noreply.github.com>
2022-08-05 17:55:59 -0400

    set folder hierchy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import styles from './styles/Home.module.css'
+import styles from '../@styles/Home.module.css'
 import Link from 'next/link';
 
 const Home: NextPage = () => {
