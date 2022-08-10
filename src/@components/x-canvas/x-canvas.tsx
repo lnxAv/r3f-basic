1c62af252fc4b4b41e11e748a2e431cdcb76bd94
Lnx <46533182+lnxAv@users.noreply.github.com>
2022-08-10 18:20:23 -0400

    add x-gui style & fix id issue

--- src/@components/x-canvas/x-canvas.tsx
+++ src/@components/x-canvas/x-canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Canvas, Props as CanvasProps } from '@react-three/fiber'
 import { XPerf } from '../x-perf/x-perf'
 import { uniqueId } from 'lodash'
@@ -11,22 +11,31 @@ interface Props {
 
 const XCanvasWrapper = styled.div`
   position: relative;
-  border: 1px solid red;
+  border: 1px solid #181c20;
+  background: #181c20;
+  color: #8c92a4;
+  font-size: 12px;
+  font-family: monospace;
   button#canvas-toggle {
     position: absolute;
     z-index: 999;
-    background: red;
+    background: #181c20;
+    padding: 1px 5px;
+    border-bottom-right-radius: 5px;
   }
 `
 
 // Offer a special canvas injected with features
 export const XCanvas: React.FC<Props & CanvasProps> = (props) => {
-  const [canvas_id] = useState<string>(uniqueId('canvas-'))
+  const [canvas_id, setCanvasId] = useState<string>('')
   const [selectedCanvas, setSelectedCanvas] = useGlobalStore((state) => [
     state.selectedCanvas,
     state.setSelectedCanvas,
   ])
 
+  useEffect(()=>{
+    setCanvasId(uniqueId('canvas-'))
+  }, [])
   const handleSelection = () => {
     setSelectedCanvas(canvas_id !== selectedCanvas ? canvas_id : '')
   }
@@ -34,7 +43,7 @@ export const XCanvas: React.FC<Props & CanvasProps> = (props) => {
   return (
     <XCanvasWrapper>
       <button id={'canvas-toggle'} onClick={handleSelection}>
-        x-perf
+        {canvas_id? canvas_id: ''}
       </button>
       <Canvas>
         <XPerf id={canvas_id} />
