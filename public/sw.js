929366136ed889e304872ecf227246e1557c8d5a
Lnx <46533182+lnxAv@users.noreply.github.com>
2022-08-18 15:01:15 -0400

    Add suspense and optm

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,122 @@
-if(!self.define){let e,s={};const n=(n,a)=>(n=new URL(n+".js",a).href,s[n]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=s,document.head.appendChild(e)}else e=n,importScripts(n),s()})).then((()=>{let e=s[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(a,c)=>{const r=e||("document"in self?document.currentScript.src:"")||location.href;if(s[r])return;let t={};const i=e=>n(e,r),o={module:{uri:r},exports:t,require:i};s[r]=Promise.all(a.map((e=>o[e]||i(e)))).then((e=>(c(...e),t)))}}define(["./workbox-6a1bf588"],(function(e){"use strict";importScripts("fallback-IZM93zHLkk9I_hz5g39_u.js"),self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"/_next/static/IZM93zHLkk9I_hz5g39_u/_buildManifest.js",revision:"1f80169b9dc03daf8fcf9ba5a3673542"},{url:"/_next/static/IZM93zHLkk9I_hz5g39_u/_ssgManifest.js",revision:"5352cb582146311d1540f6075d1f265e"},{url:"/_next/static/chunks/100-47b22838528bb251.js",revision:"47b22838528bb251"},{url:"/_next/static/chunks/243-1e61d020d7182b1d.js",revision:"1e61d020d7182b1d"},{url:"/_next/static/chunks/906-e3638568a34f2543.js",revision:"e3638568a34f2543"},{url:"/_next/static/chunks/framework-cf6f4a28bcb3ff1b.js",revision:"cf6f4a28bcb3ff1b"},{url:"/_next/static/chunks/main-6bb39b5950ca5d59.js",revision:"6bb39b5950ca5d59"},{url:"/_next/static/chunks/pages/_app-1c7674332d582ea8.js",revision:"1c7674332d582ea8"},{url:"/_next/static/chunks/pages/_error-7397496ca01950b1.js",revision:"7397496ca01950b1"},{url:"/_next/static/chunks/pages/_offline-ca4cad30169b50ce.js",revision:"ca4cad30169b50ce"},{url:"/_next/static/chunks/pages/home-3503a8e123167b72.js",revision:"3503a8e123167b72"},{url:"/_next/static/chunks/pages/index-e9ac0d1494855278.js",revision:"e9ac0d1494855278"},{url:"/_next/static/chunks/pages/test-7460a4074bcadc8f.js",revision:"7460a4074bcadc8f"},{url:"/_next/static/chunks/pages/test2-786d48bc5711a7ee.js",revision:"786d48bc5711a7ee"},{url:"/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js",revision:"837c0df77fd5009c9e46d446188ecfd0"},{url:"/_next/static/chunks/webpack-b0d7587551f18e93.js",revision:"b0d7587551f18e93"},{url:"/_next/static/css/c31eaf49efa7cfa6.css",revision:"c31eaf49efa7cfa6"},{url:"/_next/static/media/glitchalpha.24222c1e.png",revision:"4f9ec5c41fb87fb20d50ef6db73cd46c"},{url:"/_offline",revision:"IZM93zHLkk9I_hz5g39_u"},{url:"/favicon.ico",revision:"c30c7d42707a47a3f4591831641e50dc"},{url:"/locales/en/common.json",revision:"1461c30b93905d606779a8900a97ce9b"},{url:"/locales/fr/common.json",revision:"8ee3f6349f6ebd51be65d58e6a496bcd"},{url:"/manifest.json",revision:"42b077c1d05a7d6758b4691ac2aa76fb"},{url:"/three/fake_uv.jpeg",revision:"9782c8406c369d98d41eaea10db3c938"},{url:"/three/glitchalpha.png",revision:"4f9ec5c41fb87fb20d50ef6db73cd46c"},{url:"/vercel.svg",revision:"4b4f1876502eb6721764637fe5c41702"}],{ignoreURLParametersMatching:[]}),e.cleanupOutdatedCaches(),e.registerRoute("/",new e.NetworkFirst({cacheName:"start-url",plugins:[{cacheWillUpdate:async({request:e,response:s,event:n,state:a})=>s&&"opaqueredirect"===s.type?new Response(s.body,{status:200,statusText:"OK",headers:s.headers}):s},{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,new e.CacheFirst({cacheName:"google-fonts-webfonts",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:31536e3}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,new e.StaleWhileRevalidate({cacheName:"google-fonts-stylesheets",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:604800}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,new e.StaleWhileRevalidate({cacheName:"static-font-assets",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:604800}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,new e.StaleWhileRevalidate({cacheName:"static-image-assets",plugins:[new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\/_next\/image\?url=.+$/i,new e.StaleWhileRevalidate({cacheName:"next-image",plugins:[new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:mp3|wav|ogg)$/i,new e.CacheFirst({cacheName:"static-audio-assets",plugins:[new e.RangeRequestsPlugin,new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:mp4)$/i,new e.CacheFirst({cacheName:"static-video-assets",plugins:[new e.RangeRequestsPlugin,new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:js)$/i,new e.StaleWhileRevalidate({cacheName:"static-js-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:css|less)$/i,new e.StaleWhileRevalidate({cacheName:"static-style-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\/_next\/data\/.+\/.+\.json$/i,new e.StaleWhileRevalidate({cacheName:"next-data",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/\.(?:json|xml|csv)$/i,new e.NetworkFirst({cacheName:"static-data-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;const s=e.pathname;return!s.startsWith("/api/auth/")&&!!s.startsWith("/api/")}),new e.NetworkFirst({cacheName:"apis",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:16,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;return!e.pathname.startsWith("/api/")}),new e.NetworkFirst({cacheName:"others",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute((({url:e})=>!(self.origin===e.origin)),new e.NetworkFirst({cacheName:"cross-origin",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:3600}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET")}));
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-eac1af49'], (function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  importScripts("fallback-development.js");
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        request,
+        response,
+        event,
+        state
+      }) => {
+        if (response && response.type === 'opaqueredirect') {
+          return new Response(response.body, {
+            status: 200,
+            statusText: 'OK',
+            headers: response.headers
+          });
+        }
+
+        return response;
+      }
+    }, {
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
+    "cacheName": "dev",
+    plugins: [{
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+
+}));
+//# sourceMappingURL=sw.js.map
--- src/@components/x/x-canvas/component.tsx
+++ src/@components/x/x-canvas/component.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { memo, Suspense, useEffect, useMemo, useState } from 'react'
 import { Canvas, Props as CanvasProps } from '@react-three/fiber'
 import { uniqueId } from 'lodash'
 import { useGlobalStore } from '../../../@helpers/x-store'
 import { XCanvasProps } from './types'
 import XCanvasWrapper, { fullScreenStyle } from './styled'
 import dynamic from 'next/dynamic'
-import { DynamicScrollableHtml } from './scrollableHtml/component'
+import ScrollableHtml, {
+  DynamicScrollableHtml,
+} from './scrollableHtml/component'
 import { XPerf } from '../x-perf/component'
-import { Preload } from '@react-three/drei'
+import { Html, Preload, useProgress } from '@react-three/drei'
 
-export const DynamicXCanvas = dynamic<XCanvasProps & CanvasProps>(() =>
-  import('./component').then((mod) => mod.XCanvas)
+export const DynamicXCanvas = dynamic<XCanvasProps & CanvasProps>(
+  () => import('../../../@components/x/x-canvas/component'),
+  { ssr: true, suspense: true }
 )
 
 // Offer a special canvas injected with features
@@ -37,6 +40,10 @@ export const XCanvas: React.FC<XCanvasProps & CanvasProps> = ({
     // TODO : pass canvas ref instead ??
     setSelectedCanvas(canvas_id !== selectedCanvas ? canvas_id : '')
   }
+  const memoScroll = useMemo(
+    () => (html ? <ScrollableHtml {...html} /> : null),
+    [html]
+  )
 
   return (
     <XCanvasWrapper
@@ -47,11 +54,13 @@ export const XCanvas: React.FC<XCanvasProps & CanvasProps> = ({
         {canvas_id ? canvas_id : ''}
       </button>
       <Canvas {...props}>
-        <Preload />
-        {color ? <color attach='background' args={[color]} /> : null}
-        <XPerf id={canvas_id} />
-        <group>{children}</group>
-        {html ? <DynamicScrollableHtml {...html} /> : null}
+        <Suspense>
+          <Preload />
+          {color ? <color attach='background' args={[color]} /> : null}
+          <XPerf id={canvas_id} />
+          <group>{children}</group>
+          {memoScroll}
+        </Suspense>
       </Canvas>
     </XCanvasWrapper>
   )
